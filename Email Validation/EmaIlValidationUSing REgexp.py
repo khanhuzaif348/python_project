def69259d5035114d4c40813190a2bb0f4c0b663
Mohd HUzaif <103990272+khanhuzaif348@users.noreply.github.com>
2025-01-11 22:05:42 +0530

    Update EmaIlValidationUSing REgexp.py

--- Email Validation/EmaIlValidationUSing REgexp.py
+++ Email Validation/EmaIlValidationUSing REgexp.py
@@ -1,1 +1,14 @@
+#a - z Must be small  ^ - starting symbol
+# 0-9 digit
+# . _ ONetime [\._]? return 0 ,1
+#@ one [@]\w search 
+# after regexp like we find . in last 2 , 3 position then we apply [.]\w{2,3}$ $ means last string
+# . in 2nd , 3rd place
+import re
+email_cndn = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+usr_email = input("Enter Email: --   ")
+if re.search(email_cndn,usr_email):
+    print("Right Email ")
+else:
+    print("Oh you Enter Wrong email ")
 
